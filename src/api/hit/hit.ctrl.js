5bcd62a09070f4f518f5459302b31ddf582944f1
sesang06 <sesang06@naver.com>
2019-05-28 01:50:16 +0900

    쿼리, 리밋

--- src/api/hit/hit.ctrl.js
+++ src/api/hit/hit.ctrl.js
@@ -1,11 +1,20 @@
 const { LightNovel, Author, Publisher, Category } = require('../../../models');
-
+var Sequelize = require('sequelize')
 exports.list = async (ctx) => {
     try {
-        const list = await LightNovel.findAll({ include: [Author, Publisher, Category] });
-        console.log(list);
+        const Op = Sequelize.Op
+        const lastId = parseInt(ctx.query.last_id || 0, 10);
+        const list = await LightNovel.findAll({
+            include: [Author, Publisher, Category],
+            where: {
+                id: {
+                    [Op.gt]: lastId
+                }
+            },
+            limit: 10
+        });
         const body = {
-            code : 200,
+            code: 200,
             message: "Success",
             data: {
                 list: list
